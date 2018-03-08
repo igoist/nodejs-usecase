054bb4841f6aded66b7392c191fa309e4a2160d3
igoist <493512132@qq.com>
2018-03-08 14:08:04 +0800

    http demo0001 -- get(fn encapsulation, and now we have the getPageImgs to get imgs with specific pageNum. Remember the cdn and .jpeg)

--- http/demo0001/index.js
+++ http/demo0001/index.js
@@ -1,58 +1,101 @@
 /**
  * In this demo
- * use http.get to get content with the specific url, and then use the regex expression to match the pins
+ * use http.get to get content with the specific url, and then use the regex expression or cheerio to match the pins
  */
 
 const http = require('http');
 // const cheerio = require('cheerio');
 const fs = require('fs');
+const path = require('path');
 
-const url = 'http://huaban.com/search/?q=%E7%85%A7%E7%89%87%E5%A2%99&jegvhmak&page=1&per_page=20&wfl=1';
+const perPage = 20;
 
-http.get(url, res => {
+const cdnPath = 'http://img.hb.aicdn.com/';
 
-  // const { statusCode } = res;
-  // const contentType = res.headers['content-type'];
 
-  // let err;
-  // if (statusCode !== 200) {
-  //   error = new Error('请求失败。\n' +
-  //                     `状态码: ${statusCode}`);
-  // } else if (!/^application\/json/.test(contentType)) {
-  //   error = new Error('无效的 content-type.\n' +
-  //     `期望 application/json 但获取的是 ${contentType}`);
-  // }
-  // if (err) {
-  //   console.error(error.message);
-  //   // 消耗响应数据以释放内存
-  //   res.resume();
-  //   return;
-  // }
+let getPageImgs = (pageNum, perPage) => {
+  // http://huaban.com/search/?q=%E7%85%A7%E7%89%87%E5%A2%99&jegvhmak&page=1&per_page=20&wfl=1
+  let url = 'http://huaban.com/search/?q=%E7%85%A7%E7%89%87%E5%A2%99&jegvhmak&page=' + pageNum + '&per_page=' + perPage + '&wfl=1';
 
-  res.setEncoding('utf8');
+  http.get(url, res => {
 
-  let html = '';
+    // const { statusCode } = res;
+    // const contentType = res.headers['content-type'];
 
-  res.on('data', chunk => {
-    html += chunk;
-  });
+    // let err;
+    // if (statusCode !== 200) {
+    //   error = new Error('请求失败。\n' +
+    //                     `状态码: ${statusCode}`);
+    // } else if (!/^application\/json/.test(contentType)) {
+    //   error = new Error('无效的 content-type.\n' +
+    //     `期望 application/json 但获取的是 ${contentType}`);
+    // }
+    // if (err) {
+    //   console.error(error.message);
+    //   // 消耗响应数据以释放内存
+    //   res.resume();
+    //   return;
+    // }
 
-  res.on('end', () => {
-    let ret = html.match(/page\[\"pins\"\].*;/g);
-    ret = ret[0].slice(15);
-    ret = ret.slice(0, ret.length - 1);
-    fs.writeFile('map.json', JSON.stringify(JSON.parse(ret), null, 2), err => {
-      if (err) throw err;
-      console.log('The file has been saved!');
+    res.setEncoding('utf8');
+
+    let html = '';
+
+    res.on('data', chunk => {
+      html += chunk;
     });
 
-    // let $ = cheerio.load(html);
+    res.on('end', () => {
+      let ret = html.match(/page\[\"pins\"\].*;/g);
+      ret = ret[0].slice(15);
+      ret = JSON.parse(ret.slice(0, ret.length - 1));
 
-    // $('.pin.wfc').each((index, i) => {
-      // console.log(index, i);
-    // });
+      ret.map((i, index) => {
+        let tmpUrl = cdnPath + i.file.key;
+
+        downloadImg('./img/', tmpUrl);
+      });
+
+      // 将获得的 JSON 对象写入 map，暂时注释掉
+      // fs.writeFile('map.json', JSON.stringify(ret, null, 2), err => {
+      //   if (err) throw err;
+      //   console.log('The file has been saved!');
+      // });
+
+      // let $ = cheerio.load(html);
+
+      // $('.pin.wfc').each((index, i) => {
+        // console.log(index, i);
+      // });
+    });
+
+  }).on('error', (e) => {
+    console.error(`错误: ${e.message}`);
+  });;
+};
+
+let downloadImg = (imgDir, url) => {
+  http.get(url, res => {
+    let data = '';
+    res.setEncoding('binary');
+
+    res.on('data', chunk => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      let imgPath = path.resolve(imgDir, path.basename(url)) + '.jpeg';
+      fs.writeFile(imgPath, data, 'binary', err => {
+        if (err) throw err;
+
+        console.log('Image downloaded: ', path.basename(url));
+      });
+    });
+  }).on('error', (err) => {
+    console.log(err);
   });
+};
 
-}).on('error', (e) => {
-  console.error(`错误: ${e.message}`);
-});;
+for (let i = 1; i < 4; i++) {
+  getPageImgs(i, perPage);
+}
